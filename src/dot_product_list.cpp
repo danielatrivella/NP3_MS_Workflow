7dd0cfbdd98db1e7486c2239c888f8510449ca69
crisfbazz <crisfbazz@gmail.com>
2022-01-27 09:10:05 -0300

    dot_product_list: added the max_shift parameter when applying the normDotProductShift

--- src/dot_product_list.cpp
+++ src/dot_product_list.cpp
@@ -23,45 +23,27 @@ std::vector<std::vector<double>> normDotProductShiftList(
     std::vector<double> peaks_A, std::vector<double> ints_A,
     double mz_A, std::vector<std::vector<double> > peaks_B, 
     std::vector<std::vector<double> > ints_B, std::vector<double> mzs_B,
-    double bin_size) {
+    double bin_size, double max_shift) {
   // double ISO_MASS = 1.0033;  // mass (13C) - mass (12C)
-  //int min_matched_peaks = 10, min_matched_peaks_parm = 10;
   int n_B = peaks_B.size();
   std::vector<double> similarities, matches, out_normDotProductShift;
-  // if spectra A have less than 10 peaks, do not use the min matched peaks filter
-  // if (peaks_A.size() < 10) {
-  //   min_matched_peaks = 0;
-  // }
+ 
   // compare spectra A with all in B list
   for (int i = 0; i < n_B; i++) {
     double mz_diff = mz_A-mzs_B[i];
-    // double mz_diff = abs(mz_A-mzs_B[i]);
-    // double mz_diff_iso = abs(abs(mz_diff) - ISO_MASS);
-    // double mz_diff_iso2 = abs(abs(mz_diff) - 2*ISO_MASS);
-    // //minimun diff in organic molecules is the carbon mass and max to small motifs <= 100Da
-    // //except for isotopes, which the mz_diff equals ISO_MASS or 2*ISO_MASS
-    // if (mz_diff_iso > bin_size && mz_diff_iso2 > bin_size &&
-    //     (mz_diff < 12 - bin_size || mz_diff > 100 + bin_size)) {
-    //   mz_diff = 0.0;
-    // }
-    // if spectra B have less than 10 peaks, do not use the min matched peaks filter
-    // otherwise use the min matched according to spectra A num of peaks
-    // if (peaks_B[i].size() < 10) {
-    //   min_matched_peaks_parm = 0;
-    // } else {
-    //   min_matched_peaks_parm = min_matched_peaks;
-    // }
-    if (abs(mz_diff) > 200) {
+    
+    // ignore shifts greatex than the max shift allowed, default to 200 Da
+    if (abs(mz_diff) > max_shift) {
       mz_diff = 0.0;
     }
-    // TODO retrieve similarities and matched peaks in another variable
+    // retrieve similarities and matched peaks in another variable
     out_normDotProductShift = normDotProductShift(peaks_A, ints_A,
                                                    peaks_B[i],ints_B[i],
                                                     bin_size, mz_diff);
     similarities.push_back(out_normDotProductShift[0]);
     matches.push_back(out_normDotProductShift[1]);
   }
-  // TODO return similarities and matched peaks
+  // return similarities and matched peaks
   std::vector<std::vector<double> > out_sim_matches;
   out_sim_matches.push_back(similarities);
   out_sim_matches.push_back(matches);
