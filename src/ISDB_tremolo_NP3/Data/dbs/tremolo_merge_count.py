3c33c7a26ae41aeda1d231038469e1454ddc375d
crisfbazz <crisfbazz@gmail.com>
2025-04-30 17:21:11 -0300

    tremolo_merge_count: merging the tremolo result using pandas and numpy for the matching, included the InChIKey information from the tremolo identifications to the providade count tables - simplified the code

--- src/ISDB_tremolo_NP3/Data/dbs/tremolo_merge_count.py
+++ src/ISDB_tremolo_NP3/Data/dbs/tremolo_merge_count.py
@@ -2,6 +2,24 @@ from __future__ import print_function
 
 import csv
 import sys
+import pandas as pd
+import numpy as np
+
+def map_and_index(arr1, arr2):
+    """
+    Maps elements from arr1 to their indices in arr2,
+    returning an array of indices with the order of arr2.
+
+    Args:
+        arr1 (np.ndarray): The array whose elements are to be mapped.
+        arr2 (np.ndarray): The array defining the desired order of indices.
+
+    Returns:
+        np.ndarray: An array of indices, where each element at index `i`
+                    corresponds to the index of arr2[i] in arr1.
+    """
+    mapping = {val: idx for idx, val in enumerate(arr1)}
+    return np.array([mapping[val] for val in arr2])
 
 csv.field_size_limit(sys.maxsize)
 
@@ -19,6 +37,7 @@ with open(result_file, 'rt') as f:
     result_smile = {}
     result_id = {}
     result_cn = {}
+    result_inchikey = {}
     result_score = {}
     result_ppmError = {}
     result_sharedPeaks = {}
@@ -43,6 +62,7 @@ with open(result_file, 'rt') as f:
             scan_id_pos = row.index('msclusterID')
             unpd_id_pos = row.index('CompoundName')
             smiles_id_pos = row.index('SMILES')
+            inchikey_id_pos = row.index('InChIKey')
             cas_id_pos = row.index('CAS')
             cn_id_pos = row.index('chemicalNames')
             mf_id_pos = row.index('molecularFormula')
@@ -71,6 +91,7 @@ with open(result_file, 'rt') as f:
                     continue
                 result_smile[row[scan_id_pos]] += "," + row[smiles_id_pos]
                 result_cn[row[scan_id_pos]] += ";" + row[cn_id_pos].replace(";", ":")
+                result_inchikey[row[scan_id_pos]] += ";" + row[inchikey_id_pos]
                 result_mf[row[scan_id_pos]] += ";" + row[mf_id_pos]
                 result_mw[row[scan_id_pos]] += ";" + row[mw_id_pos]
                 result_cas[row[scan_id_pos]] += ";" + row[cas_id_pos]
@@ -92,6 +113,7 @@ with open(result_file, 'rt') as f:
             else:
                 result_smile[row[scan_id_pos]] = row[smiles_id_pos]
                 result_cn[row[scan_id_pos]] = row[cn_id_pos].replace(";", ":")
+                result_inchikey[row[scan_id_pos]] = row[inchikey_id_pos]
                 result_mf[row[scan_id_pos]] = row[mf_id_pos]
                 result_mw[row[scan_id_pos]] = row[mw_id_pos]
                 result_cas[row[scan_id_pos]] = row[cas_id_pos]
@@ -111,250 +133,44 @@ with open(result_file, 'rt') as f:
                 result_npa_origin_species[row[scan_id_pos]] = row[npa_origin_species_pos]
                 scan_count[row[scan_id_pos]] = 1
 
-count_file = sys.argv[3]
 
-print("  Merging to the count files result")
+print("  Merging the tremolo result to the count tables files")
+for count_file in sys.argv[3:]:
+    # make the merging with pandas, replace previous identification result
+    count_table = pd.read_csv(count_file, low_memory=False)
+    # convert the msclusterIDs to str to allow mapping them with the keys of the dictionaries of the tremolo data
+    msclusterIDs_mapping = map_and_index(count_table.msclusterID.astype(str).values, list(result_id.keys()))
 
-with open(count_file, 'rt') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = False
-    output = []
-    for row in reader:
-        if not header:
-            cluster_index = row.index('msclusterID')
-            header = row + ["tremolo_UNPD_IDs"] + ["tremolo_SMILES"] + ["tremolo_chemicalNames"] + \
-                     ["tremolo_molecularFormula"] + ["tremolo_molecularWeight"] + ["tremolo_CAS"] + ["tremolo_MQScore"]+ \
-                     ["tremolo_mzErrorPPM"] + ["tremolo_numSharedPeaks"] + ["tremolo_NPClassifier_superclass"] + \
-                     ["tremolo_NPClassifier_class"] + ["tremolo_ClassyFire_subclass"] + ["tremolo_NPClassifier_pathway"] + \
-                     ["tremolo_NPClassifier_isglycoside"] + ["tremolo_NPAtlas_id"] + ["tremolo_NPAtlas_compound_names"] + \
-                     ["tremolo_NPAtlas_origin_type"] + ["tremolo_NPAtlas_genus"] + ["tremolo_NPAtlas_origin_species"]
-        else:
-            temp = row
-
-            if row[cluster_index] in result_id:
-                temp += [result_id[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_smile:
-                temp += [result_smile[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_cn:
-                temp += [result_cn[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_mf:
-                temp += [result_mf[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_mw:
-                temp += [result_mw[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_cas:
-                temp += [result_cas[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_score:
-                temp += [result_score[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_ppmError:
-                temp += [result_ppmError[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_sharedPeaks:
-                temp += [result_sharedPeaks[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npc_superclass:
-                temp += [result_npc_superclass[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npc_class:
-                temp += [result_npc_class[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_cf_subclass:
-                temp += [result_cf_subclass[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npc_pathway:
-                temp += [result_npc_pathway[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npc_isglycoside:
-                temp += [result_npc_isglycoside[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npa_id:
-                temp += [result_npa_id[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npa_compound_names:
-                temp += [result_npa_compound_names[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npa_origin_type:
-                temp += [result_npa_origin_type[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npa_genus:
-                temp += [result_npa_genus[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            if row[cluster_index] in result_npa_origin_species:
-                temp += [result_npa_origin_species[row[cluster_index]]]
-            else:
-                temp += ["NA"]
-
-            output += [temp]
-
-print("    - Outputing ", count_file)
-with open(count_file, 'w') as tsvfile:
-    writer = csv.writer(tsvfile, delimiter=',',
-                        quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(header)
-    for row in output:
-        writer.writerow(row)
-
-# also writes output for other count files (e.g. spectra count)
-if len(sys.argv) == 5:
-    count_file = sys.argv[4]
-    print("    - Outputing ", count_file)
-    with open(count_file, 'rt') as f:
-        reader = csv.reader(f, delimiter=',')
-        header = False
-        output = []
-        for row in reader:
-            if not header:
-                cluster_index = row.index('msclusterID')
-                header = row + ["tremolo_UNPD_IDs"] + ["tremolo_SMILES"] + ["tremolo_chemicalNames"] + \
-                         ["tremolo_molecularFormula"] + ["tremolo_molecularWeight"] + ["tremolo_CAS"] + ["tremolo_MQScore"] + \
-                         ["tremolo_mzErrorPPM"] + ["tremolo_numSharedPeaks"] + ["tremolo_NPClassifier_superclass"] + \
-                         ["tremolo_NPClassifier_class"] + ["tremolo_ClassyFire_subclass"] + ["tremolo_NPClassifier_pathway"] + \
-                         ["tremolo_NPClassifier_isglycoside"] + ["tremolo_NPAtlas_id"] + ["tremolo_NPAtlas_compound_names"] + \
-                         ["tremolo_NPAtlas_origin_type"] + ["tremolo_NPAtlas_genus"] + ["tremolo_NPAtlas_origin_species"]
-            else:
-                temp = row
-
-                if row[cluster_index] in result_id:
-                    temp += [result_id[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_smile:
-                    temp += [result_smile[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_cn:
-                    temp += [result_cn[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_mf:
-                    temp += [result_mf[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_mw:
-                    temp += [result_mw[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_cas:
-                    temp += [result_cas[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_score:
-                    temp += [result_score[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_ppmError:
-                    temp += [result_ppmError[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_sharedPeaks:
-                    temp += [result_sharedPeaks[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npc_superclass:
-                    temp += [result_npc_superclass[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npc_class:
-                    temp += [result_npc_class[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_cf_subclass:
-                    temp += [result_cf_subclass[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npc_pathway:
-                    temp += [result_npc_pathway[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npc_isglycoside:
-                    temp += [result_npc_isglycoside[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npa_id:
-                    temp += [result_npa_id[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npa_compound_names:
-                    temp += [result_npa_compound_names[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npa_origin_type:
-                    temp += [result_npa_origin_type[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npa_genus:
-                    temp += [result_npa_genus[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                if row[cluster_index] in result_npa_origin_species:
-                    temp += [result_npa_origin_species[row[cluster_index]]]
-                else:
-                    temp += ["NA"]
-
-                output += [temp]
-    with open(count_file, 'w') as tsvfile:
-        writer = csv.writer(tsvfile, delimiter=',',
-                            quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(header)
-        for row in output:
-            writer.writerow(row)
+    if "tremolo_UNPD_IDs" in count_table:
+        print("    - Overwriting previous tremolo identification result")
+        count_table = count_table.drop(["tremolo_UNPD_IDs","tremolo_SMILES","tremolo_chemicalNames","tremolo_molecularFormula",
+                          "tremolo_molecularWeight","tremolo_CAS","tremolo_MQScore","tremolo_mzErrorPPM",
+                          "tremolo_numSharedPeaks","tremolo_NPClassifier_superclass","tremolo_NPClassifier_class",
+                          "tremolo_ClassyFire_subclass","tremolo_NPClassifier_pathway",
+                          "tremolo_NPClassifier_isglycoside","tremolo_NPAtlas_id","tremolo_NPAtlas_compound_names",
+                          "tremolo_NPAtlas_origin_type","tremolo_NPAtlas_genus","tremolo_NPAtlas_origin_species"],
+                         axis=1, errors='ignore')
+    # set the tremolo values to the count_table columns
+    count_table.loc[msclusterIDs_mapping,"tremolo_UNPD_IDs"] = list(result_id.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_SMILES"] = list(result_smile.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_chemicalNames"] = list(result_cn.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_InChIKey"] = list(result_inchikey.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_molecularFormula"] = list(result_mf.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_molecularWeight"] = list(result_mw.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_CAS"] = list(result_cas.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_MQScore"] = list(result_score.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_mzErrorPPM"] = list(result_ppmError.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_numSharedPeaks"] = list(result_sharedPeaks.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPClassifier_superclass"] = list(result_npc_superclass.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPClassifier_class"] = list(result_npc_class.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_ClassyFire_subclass"] = list(result_cf_subclass.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPClassifier_pathway"] = list(result_npc_pathway.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPClassifier_isglycoside"] = list(result_npc_isglycoside.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPAtlas_id"] = list(result_npa_id.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPAtlas_compound_names"] = list(result_npa_compound_names.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPAtlas_origin_type"] = list(result_npa_origin_type.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPAtlas_genus"] = list(result_npa_genus.values())
+    count_table.loc[msclusterIDs_mapping,"tremolo_NPAtlas_origin_species"] = list(result_npa_origin_species.values())
+    # write output file
+    print("    - Outputting ", count_file)
+    count_table.to_csv(count_file, index=False)
