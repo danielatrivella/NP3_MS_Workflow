9fd239cabfbacddf7543b301951d811847cd8651
crisfbazz <crisfbazz@gmail.com>
2026-04-29 09:00:36 -0300

    library_summary_enrichment_adm: added the compressing of the library summary table with annotations using the best compression of gzip

--- src/GNPS_LibrarySearch/data/library_summary_enrichment_adm.sh
+++ src/GNPS_LibrarySearch/data/library_summary_enrichment_adm.sh
@@ -4,3 +4,5 @@
 # then the library search result can be enriched offline with all these retrieved info - workaround to execute np3 offline and remove dependency to the GNPS server for annotation enrichment
 # this should be updated by the adm everytime the Library updates (new spectrumIDs are added) - a warning will show up in the joining step from NP3 setup
 python ../GNPS2_LibrarySearch_Workflow/getGNPS_library_summary_annotations.py library_summary_ALL_GNPS_NO_PROPOGATED.tsv library_summary_ALL_GNPS_NO_PROPOGATED_annotations.tsv --numthreads 300
+# compress resulting table
+tar -cf - library_summary_ALL_GNPS_NO_PROPOGATED_annotations.tsv | gzip -9 > library_summary_ALL_GNPS_NO_PROPOGATED_annotations.tsv.tar.gz
