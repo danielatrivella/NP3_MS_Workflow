b083948a1cb623359ba3c27de94c971d892be5f5
crisfbazz <crisfbazz@gmail.com>
2026-06-17 16:26:06 -0300

    np3_workflow: incremented current version to 1.4.1

--- np3_workflow.js
+++ np3_workflow.js
@@ -1649,7 +1649,7 @@ function defaultModelDir() {
 }
 
 program
-    .version('1.4.0',  '--version')
+    .version('1.4.1',  '--version')
     .usage(' command [options]\n\n' +
         'The NP3 MS workflow is a software system with a collection of scripts to enhance untargeted metabolomics ' +
         'research focused on drug discovery with optimizations towards natural products. \n\n' +
