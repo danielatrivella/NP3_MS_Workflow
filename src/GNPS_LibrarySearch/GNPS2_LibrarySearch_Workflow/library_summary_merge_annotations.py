e3325bb8ffdff99feb552eb8fde31f46aba39a9b
crisfbazz <crisfbazz@gmail.com>
2026-07-07 22:24:39 -0300

    library_summary_merge_annotations: added a routine to valided the SMILES strings retrieved from the GNPS annotations, remove any invalid SMILES and print the count of smiles by spectra and unique molecules at the end; also tries to get the SMILES from the smiles columns for NA Smiles

--- src/GNPS_LibrarySearch/GNPS2_LibrarySearch_Workflow/library_summary_merge_annotations.py
+++ src/GNPS_LibrarySearch/GNPS2_LibrarySearch_Workflow/library_summary_merge_annotations.py
@@ -3,6 +3,19 @@ import pandas as pd
 import os, sys
 import numpy as np
 
+from rdkit import Chem, RDLogger
+from rdkit import RDLogger
+# Disable RDKit logging msgs
+RDLogger.DisableLog('rdApp.*')
+
+# validate smiles string, MolFromSmiles returns None if the string is invalid
+def is_valid_smiles(smiles_string):
+    if pd.isna(smiles_string):
+        return False
+    mol = Chem.MolFromSmiles(smiles_string)
+    return mol is not None
+
+
 def join_library_summary_annotations(library_summary_file, library_summary_annotations_file, output_filename):
     # Trying to load the library summary
     if not os.path.exists(library_summary_file):
@@ -39,6 +52,49 @@ def join_library_summary_annotations(library_summary_file, library_summary_annot
     if library_summary_enriched.shape[0] != library_summary.shape[0]:
         sys.exit("The library summary enrichment with the summary annotations failed, some Spectrum IDs went missing after the match.")
     
+    # validate SMILES list, remove invalid SMILES from the "Smiles" column and then
+    # # for the invalid ones check if there is a valid Smiles string in the "smiles" column
+    invalid_smiles = 0
+    print("- Validating the Smiles string and removing invalid SMILES...")
+    check_Smiles = pd.DataFrame({'Smiles':library_summary_enriched.Smiles.unique(), 'Smiles_valid':np.nan})
+    for i in range(check_Smiles.shape[0]):
+        check_Smiles_is_valid = is_valid_smiles(check_Smiles.loc[i, "Smiles"])
+        if not check_Smiles_is_valid:
+            check_Smiles.loc[i, "Smiles_valid"] = np.nan
+            invalid_smiles += 1
+        else:
+            check_Smiles.loc[i, "Smiles_valid"] = check_Smiles.loc[i, "Smiles"]
+    # merge the checked smiles with the library summary
+    library_summary_enriched = library_summary_enriched.merge(check_Smiles, on='Smiles', how='left')
+    # remove invalid Smiles
+    library_summary_enriched.loc[:,'Smiles'] = library_summary_enriched.loc[:,'Smiles_valid']
+    library_summary_enriched.drop(['Smiles_valid'], axis=1, inplace=True)
+    print("  - Removed a total of", invalid_smiles-1, " unique invalid SMILES string.") # remove one from NA
+    # for the invalid Smiles, check if there is a valid SMILES in the "smiles" column
+    # this is a second try for the invalid Smiles
+    check_Smiles = pd.DataFrame({'smiles': library_summary_enriched.loc[(library_summary_enriched.Smiles.isna() & ~library_summary_enriched.smiles.isna()),
+                                                                        'smiles'].unique(),
+                                 'smiles_valid': np.nan})
+    for i in range(check_Smiles.shape[0]):
+        check_Smiles_is_valid = is_valid_smiles(check_Smiles.loc[i,"smiles"])
+        if check_Smiles_is_valid:
+            check_Smiles.loc[i,"smiles_valid"] = check_Smiles.loc[i,"smiles"]
+    # if any smiles from the invalid ones was validated, replace them in the Smiles column where it had NAs
+    if (~check_Smiles.smiles_valid.isna()).any():
+        library_summary_enriched = library_summary_enriched.merge(check_Smiles, on='smiles', how='left')
+        check_Smiles_is_valid = (~library_summary_enriched.smiles_valid.isna() & library_summary_enriched.Smiles.isna())
+        library_summary_enriched.loc[check_Smiles_is_valid, 'Smiles'] = library_summary_enriched.loc[check_Smiles_is_valid, 'smiles_valid']
+        library_summary_enriched.drop(['smiles_valid'], axis=1, inplace=True)
+    
+    revalidated_smiles = library_summary_enriched.loc[check_Smiles_is_valid, 'Smiles'].unique().size
+    print("  - Recovered a total of ",revalidated_smiles, " unique SMILES string.")
+    print("  - Total number of spectra present in the GNPS library summary =",library_summary_enriched.shape[0])
+    print("  - Total number of spectra with a valid SMILES =", (~library_summary_enriched.Smiles.isna()).sum(),
+          "(",round((~library_summary_enriched.Smiles.isna()).sum()/library_summary_enriched.shape[0]*100),"%)")
+    print("  - Total number of unique SMILES present in the GNPS library summary =",
+          library_summary_enriched.Smiles.unique().size - 1)  # remove one NA
+    
+    # store the library summary enriched and validated
     library_summary_enriched.to_csv(output_filename, sep="\t", index=False)
 
 def main():
