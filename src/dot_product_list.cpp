e4b9201412662ec4829b6c585032b237d4fc16eb
crisfbazz <crisfbazz@gmail.com>
2026-07-31 19:06:22 -0300

    dot_product_list: updated the normDotProductShiftList func to return only this similarity values >= 0.1, do not return zero values

--- src/dot_product_list.cpp
+++ src/dot_product_list.cpp
@@ -18,6 +18,11 @@ std::vector<double> normDotProductList(std::vector<double> peaks_A, std::vector<
   return (similarities);
 }
 
+// performs the normDotProductShif between spec A and the list of spec B
+// only returns the similarity values >= 0.1
+// return a list with 3 items:
+// (0) the list of valid cosines (>=0.1), (1) the list with their number of matches 
+// and (2) the list with their index+1 in spec B list (R format - idx starts in 1 and not in 0)
 // [[Rcpp::export]]
 std::vector<std::vector<double>> normDotProductShiftList(
     std::vector<double> peaks_A, std::vector<double> ints_A,
@@ -26,31 +31,36 @@ std::vector<std::vector<double>> normDotProductShiftList(
     double bin_size, double max_shift) {
   // double ISO_MASS = 1.0033;  // mass (13C) - mass (12C)
   int n_B = peaks_B.size();
-  std::vector<double> similarities, matches, out_normDotProductShift;
+  std::vector<double> similarities, matches, out_normDotProductShift,valid_sim_idx;
  
   // compare spectra A with all in B list
   for (int i = 0; i < n_B; i++) {
     double mz_diff = mz_A-mzs_B[i];
     
-    // ignore shifts greatex than the max shift allowed, default to 200 Da
+    // ignore shifts greater than the max shift allowed, default to 200 Da
     if (abs(mz_diff) > max_shift) {
       mz_diff = 0.0;
     }
-    // retrieve similarities and matched peaks in another variable
+    // retrieve similarity value - cosine - and number of matched peaks 
     out_normDotProductShift = normDotProductShift(peaks_A, ints_A,
                                                    peaks_B[i],ints_B[i],
                                                     bin_size, mz_diff);
-    if (out_normDotProductShift[0] < 0.1) {
-      out_normDotProductShift[0] = 0.0;
-      out_normDotProductShift[1] = 0;
+    // check if the similarity value (first idx) is greater or equal than the cut of 0.1
+    // if yes, return it and add the current spec B idx to the valid list, 
+    // otherwise ignore it - which is equal to setting it to zero
+    if (out_normDotProductShift[0] >= 0.1) {
+      similarities.push_back(out_normDotProductShift[0]);
+      matches.push_back(out_normDotProductShift[1]);
+      valid_sim_idx.push_back(i+1);
     }
-      
-    similarities.push_back(out_normDotProductShift[0]);
-    matches.push_back(out_normDotProductShift[1]);
+    // otherwise dont return the sim and the matches:
+    //out_normDotProductShift[0] = 0.0;
+    //out_normDotProductShift[1] = 0;
   }
   // return similarities and matched peaks
   std::vector<std::vector<double> > out_sim_matches;
   out_sim_matches.push_back(similarities);
   out_sim_matches.push_back(matches);
+  out_sim_matches.push_back(valid_sim_idx);
   return (out_sim_matches);
 }
