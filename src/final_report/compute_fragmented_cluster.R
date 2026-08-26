1c0f31fcfefcd9186857b654d96db4e036df1470
crisfbazz <crisfbazz@gmail.com>
2026-08-26 10:43:37 -0300

    compute_fragmented_cluster: add parm to define the trailing spaces of the output msgs; added parm to control a criteria to search for fragmetned clusters only in the mz with numJoins > 0 (used in the clean rounds); and added execution time measurement

--- src/final_report/compute_fragmented_cluster.R
+++ src/final_report/compute_fragmented_cluster.R
@@ -6,17 +6,26 @@ RMSE <- function(x, y) {
   sqrt(mean((x - y)^2))
 }
 
+# TODO make this function with data.table - benchmark time gain
+
 # compute the number of fragmented clusters present in the provided count table and
 # return a column with the putative fragmented clusters of each m/z by row (same order of the provided table) 
 # assigned as follow:
 # 0 for a not fragmented cluster m/z; 1 or greater for a m/z that have these number of fragmented clusters;
 # and -1 for a m/z signalized as a fragmented cluster
-# where putative_origin indicates if the column with the msclusterID of the m/z 
+# where 
+# @putative_origin indicates if the column with the msclusterID of the m/z 
 # assigned as the putative origin cluster of the detected fragmented clusters should be returned
 # if FALSE only returns the vector with the putative fragmented clusters assigned as described above
-compute_fragmented_clusters <- function(clean_count_table, rt_tol=2, mz_tol=0.025, putative_origin = FALSE, table_type="Step 5 clean") {
-  cat("\n  - Computing the number of fragmented cluster in the",table_type,"table\n")
-    
+# @with_num_joins indicates to search for fragmented clusters only in the rows where num_joins > 0 (used in the clean step)
+compute_fragmented_clusters <- function(clean_count_table, 
+                                        rt_tol=2, mz_tol=0.025, 
+                                        putative_origin = FALSE, 
+                                        table_type="Step 5 clean",
+                                        with_numJoins=FALSE,
+                                        add_space_verbose="") {
+  cat("\n",add_space_verbose,"* Computing the number of fragmented cluster in the",table_type,"table *\n")
+  t1 <- Sys.time()  
   if ('BLANKS_TOTAL' %in% names(clean_count_table)) {
     blanks_flag <- TRUE
   } else {
@@ -29,6 +38,9 @@ compute_fragmented_clusters <- function(clean_count_table, rt_tol=2, mz_tol=0.02
   for (i in seq_len(nrow(clean_count_table))) {
     # print(i)
     if (clean_count_table$fragmented_clusters[[i]] < 0) {
+      next()
+    }
+    if (with_numJoins && clean_count_table$numJoins[[i]] == 0) {
       next()
     }
     
@@ -66,23 +78,17 @@ compute_fragmented_clusters <- function(clean_count_table, rt_tol=2, mz_tol=0.02
   n <- nrow(clean_count_table)
   number_fragmented_clusters <- sum(clean_count_table$fragmented_clusters<0)
   number_mzs_w_fragmented_clusters <- sum(clean_count_table$fragmented_clusters>0)
-  cat("\n    - Total number of m/zs that had at least one putative fragmented clusters and percentage overall m/zs:", 
+  cat("\n",add_space_verbose,"  - Total number of m/zs that had at least one putative fragmented clusters and percentage overall m/zs:", 
       number_mzs_w_fragmented_clusters, "(",round(100*number_mzs_w_fragmented_clusters/nrow(clean_count_table),2), "% ) \n")
-  cat("\n    - Total number of putative fragmented clusters and percentage overall m/zs:", 
+  cat("",add_space_verbose,"  - Total number of putative fragmented clusters and percentage overall m/zs:", 
       number_fragmented_clusters, "(",round(100*number_fragmented_clusters/nrow(clean_count_table),2), "% ) \n")
   
+  t2 <- Sys.time()
+  cat("",add_space_verbose,"  * Done in", round(t2-t1, 2), units(t2-t1), "*\n")
   # return the resulting fragmented clusters column to be added to the clean tables
   if (putative_origin) {
     return(clean_count_table[,c("fragmented_clusters","origin_cluster")])
   } else {
     return(clean_count_table$fragmented_clusters)
   }
-  #write_csv(clean_count_table, sub(".csv","_fragmentedClusters.csv", count_table_path))
 }
-
-# TODO make this function with data.table - benchmark time gain
-# TODO make this function for only a selection of m/zs (the ones with a join (modified ones))
-
-# TODO try clustering the origin cluster with the fragmented clusters, than 
-# if more than 1 fragmented cluster remaining, 
-# also try to cluster them between each other
